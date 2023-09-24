4bf97113f3a8fef4c5efc55218dd4a69375e450a
Ngoc Tran <phuongngoc@gmail.com>
2023-09-24 21:17:16 +0700

    get source frame

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,20 +1,40 @@
 console.log("Hi, I'm Responsivfy Helper");
 let currentUrl = "";
 
+function getWindowName() {
+  // console.log("getWindowName", window.name);
+  return window.name;
+}
+
+async function getFrameName(tabId, frameId) {
+  try {
+    const a = await chrome.scripting.executeScript({
+      target: { tabId, frameIds: [frameId] },
+      func: getWindowName,
+    });
+    // console.log("script injected on target frames", a[0].result);
+    return a[0].result;
+  } catch (e) {
+    // console.error("script NOT injected on target frames", tabId, frameId, e);
+  }
+}
+
 /**
  * @param {number} tabId
  * @param {string} url
  */
-async function navigateFramesToUrl(tabId, url) {
+async function navigateFramesToUrl(tabId, fromFrameId, url) {
   if (currentUrl === url) {
     return;
   }
   currentUrl = url;
 
+  const frameName = await getFrameName(tabId, fromFrameId);
+
   try {
     await chrome.tabs.sendMessage(
       tabId,
-      { event: "url_changed", url },
+      { event: "url_changed", url, sourceFrame: frameName },
       { frameId: 0 }
     );
   } catch (e) {
@@ -27,7 +47,7 @@ chrome.webNavigation.onCompleted.addListener(function (details) {
     // navigated to new url in iframe
     // console.log("navigation completed", details.url, details);
 
-    navigateFramesToUrl(details.tabId, details.url);
+    navigateFramesToUrl(details.tabId, details.frameId, details.url);
   }
 });
 
@@ -38,10 +58,10 @@ chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
 
     if (details.transitionType === "auto_subframe") {
       // navigate to new url
-      navigateFramesToUrl(details.tabId, details.url);
+      navigateFramesToUrl(details.tabId, details.frameId, details.url);
     } else if (details.transitionType === "manual_subframe") {
       // pushed state to new url
-      navigateFramesToUrl(details.tabId, details.url);
+      navigateFramesToUrl(details.tabId, details.frameId, details.url);
     }
   }
 });
@@ -53,6 +73,7 @@ chrome.runtime.onMessageExternal.addListener(function (
 ) {
   if (request?.command === "get_settings") {
     return sendResponse({
+      version: chrome.runtime.getManifest().version,
       enabled: true,
     });
   }
